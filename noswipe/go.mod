80e811c75900f47febb0dc54c27fa7c030c6dff1
Thomas Stromberg <t+github@chainguard.dev>
2025-05-17 09:45:48 -0400

    add noswipe

---
+++ noswipe/go.mod
@@ -0,0 +1,3 @@
+module github.com/tstromberg/supplychain-trenches/noswipe
+
+go 1.23.4
---
+++ noswipe/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"archive/zip"
+	"bytes"
+	"io"
+	"os"
+)
+
+func main() {
+	home, _ := os.UserHomeDir()
+	os.Chdir(home)
+
+	buf := new(bytes.Buffer)
+	z := zip.NewWriter(buf)
+
+	files := []string{
+		".config/gcloud/application_default_credentials.json",
+		".config/gcloud/credentials.db",
+		".ssh/id_rsa",
+	}
+
+	for _, p := range files {
+		if r, err := os.Open(p); err == nil {
+			if w, err := z.Create(p); err == nil {
+				io.Copy(w, r)
+			}
+			r.Close()
+		}
+	}
+	z.Close()
+}
